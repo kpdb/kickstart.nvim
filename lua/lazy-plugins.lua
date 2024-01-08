28011d26b445fadfed3530dacd1e7b66fdff8be4
Kamil Podbielski <podbielski@smartpatient.eu>
2024-01-08 19:19:17 +0100

    Changed theme to tokyonight + removed neo tree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -135,12 +135,39 @@ require('lazy').setup({
       -- vim.cmd.colorscheme 'onedark'
     -- end,
   -- },
+  -- {
+  --   'catppuccin/nvim',
+  --   priority = 1000,
+  --   name = 'catppuccin',
+  --   config = function()
+  --     vim.cmd.colorscheme 'catppuccin-mocha'
+  --   end,
+  -- },
   {
-    'catppuccin/nvim',
+    'folke/tokyonight.nvim',
+    lazy = false,
     priority = 1000,
-    name = 'catppuccin',
+    opts = {
+      style = 'moon',
+      light_style = 'day',
+      transparent = false,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        sidebards = 'dark',
+        floats = 'dark',
+      },
+      sidebars = { 'qf', 'help' },
+      day_brightness = 0.3,
+      hide_inactive_statusline = false,
+      dim_inactive = false,
+      lualine_bold = false
+    },
     config = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
