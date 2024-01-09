cd2541b24fa2df1e91dea055f493759fa28ef5e4
Kamil Podbielski <freeniac@gmail.com>
2024-01-09 09:20:57 +0100

    Finetuning rust support

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,7 +1,14 @@
 return {
-  "rust-lang/rust.vim",
-  ft = "rust",
-  init = function ()
-    vim.g.rustfmt_autosave = 1
-  end
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = { 'rust' },
+  },
 }
