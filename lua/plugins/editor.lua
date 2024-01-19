9662c0a2fbc6114e2cd9285c35a1485c7b30f038
Kamil Podbielski <podbielski@smartpatient.eu>
2024-01-19 17:49:25 +0100

    Added cloak for masking env variables

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -144,4 +144,22 @@ return {
       }
     end,
   },
+  {
+    "laytan/cloak.nvim",
+    config = function()
+      require("cloak").setup({
+        enabled = true,
+        cloak_character = "*",
+        highlight_group = "Comment",
+        patterns = {
+          {
+            file_pattern = {
+              ".env",
+            },
+            cloak_pattern = "=.*",
+          },
+        },
+      })
+    end,
+  },
 }
